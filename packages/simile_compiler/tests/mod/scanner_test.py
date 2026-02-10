3015402c42eecb89f19f4bbf5ab8d3a67ea0fc24
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2026-02-09 22:05:10 -0500

    start revising tests

---
+++ packages/simile_compiler/tests/mod/scanner_test.py
@@ -0,0 +1,362 @@
+import pytest
+import hypothesis
+
+from src.mod.scanner import (
+    scan,
+    OPERATOR_TOKEN_TABLE,
+    KEYWORD_TABLE,
+    TokenType,
+    Token,
+    Location,
+)
+
+
+# MARK: Setup
+TOKENS_AND_KEYWORDS = list(OPERATOR_TOKEN_TABLE.items()) + list(KEYWORD_TABLE.items())
+KEYWORDS_WITH_SPACE = list(filter(lambda item: " " in item, KEYWORD_TABLE.keys()))
+
+
+def get_token_types(input_: str) -> list[TokenType]:
+    return list(map(lambda tk: tk.type_, scan(input_)))
+
+
+def assert_language_invariants(tokens: list[TokenType]):
+    assert tokens.count(TokenType.INDENT) == tokens.count(TokenType.DEDENT)
+    if tokens:
+        assert tokens[0] != TokenType.EOF
+        assert tokens[-1] == TokenType.EOF
+
+
+def assert_token_types_equal(actual: list[TokenType], expected: list[TokenType]):
+    expected.append(TokenType.NEWLINE)
+    expected.append(TokenType.EOF)
+    assert len(actual) == len(expected)
+    for actual_token, expected_type in zip(actual, expected):
+        assert actual_token == expected_type
+
+
+def assert_tokens_equal(actual: list[Token], expected: list[Token]):
+    assert len(actual) == len(expected)
+    for actual_token, expected_token in zip(actual, expected):
+        assert actual_token.type_ == expected_token.type_
+        assert actual_token.value == expected_token.value
+        assert actual_token.start_location == expected_token.start_location
+        assert actual_token.end_location == expected_token.end_location
+
+
+# MARK: Happy path
+@pytest.mark.parametrize(
+    "input_, expected",
+    TOKENS_AND_KEYWORDS,
+)
+def test_single_symbol(input_: str, expected: TokenType):
+    actual_tokens = get_token_types(input_)
+    assert_language_invariants(actual_tokens)
+    assert_token_types_equal(actual_tokens, [expected])
+
+
+@pytest.mark.parametrize(
+    "input_fst, expected_fst",
+    TOKENS_AND_KEYWORDS,
+)
+@pytest.mark.parametrize(
+    "input_snd, expected_snd",
+    TOKENS_AND_KEYWORDS,
+)
+def test_double_symbols_with_space(
+    input_fst: str,
+    expected_fst: TokenType,
+    input_snd: str,
+    expected_snd: TokenType,
+):
+    input_ = f"{input_fst} {input_snd}"
+    actual_tokens = get_token_types(input_)
+    assert_language_invariants(actual_tokens)
+
+    expected = [expected_fst, expected_snd]
+    if input_ in KEYWORDS_WITH_SPACE:
+        # if the input is a keyword with an optional space,
+        # only expect one keyword
+        expected = [KEYWORD_TABLE[input_]]
+
+    assert_token_types_equal(actual_tokens, expected)
+
+
+@pytest.mark.parametrize(
+    "input_, expected",
+    [
+        ("\n", []),
+        ("", []),
+        (" ", []),
+        (" \n", []),
+        ("\t\n\t", []),
+        ("\t", []),
+    ],
+)
+def test_ignore_whitespace(input_: str, expected: list[TokenType]):
+    actual_tokens = get_token_types(input_)
+    assert_language_invariants(actual_tokens)
+    assert_token_types_equal(actual_tokens, expected)
+
+
+@pytest.mark.parametrize(
+    "input_, expected",
+    [
+        ("< .", [TokenType.LT, TokenType.DOT, TokenType.NEWLINE]),
+        (
+            " < .",
+            [
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.DOT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+            ],
+        ),
+        (
+            "  < .",
+            [
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.DOT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+            ],
+        ),
+        (
+            "   < .",
+            [
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.DOT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+            ],
+        ),
+        (
+            "    < .",
+            [
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.DOT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+            ],
+        ),
+        (
+            "\t< .",
+            [
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.DOT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+            ],
+        ),
+        (
+            "<\n\t<\n<",
+            [
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+            ],
+        ),
+        (
+            "<\n <\n<",
+            [
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+            ],
+        ),
+        (
+            "<\n <\n  <\n",
+            [
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+                TokenType.DEDENT,
+            ],
+        ),
+        (
+            "<\n <\n  <\n<",
+            [
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.INDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+                TokenType.DEDENT,
+                TokenType.LT,
+                TokenType.NEWLINE,
+            ],
+        ),
+    ],
+)
+def test_indentation(input_: str, expected: list[TokenType]):
+    actual_tokens = get_token_types(input_)
+    assert_language_invariants(actual_tokens)
+    assert_token_types_equal(actual_tokens, expected)
+
+
+@pytest.mark.parametrize(
+    "input_, expected",
+    [
+        ("test", [TokenType.IDENTIFIER, TokenType.NEWLINE]),
+        ("\ttest", [TokenType.INDENT, TokenType.IDENTIFIER, TokenType.NEWLINE, TokenType.DEDENT]),
+        ("test: int", [TokenType.IDENTIFIER, TokenType.COLON, TokenType.IDENTIFIER, TokenType.NEWLINE]),
+        (
+            "test: int1\ntest:int2",
+            [
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.IDENTIFIER,
+                TokenType.NEWLINE,
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.IDENTIFIER,
+                TokenType.NEWLINE,
+            ],
+        ),
+        (
+            "\ttest: int1\n\ttest:int2",
+            [
+                TokenType.INDENT,
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.IDENTIFIER,
+                TokenType.NEWLINE,
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.IDENTIFIER,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+            ],
+        ),
+    ],
+)
+def test_identifiers(input_: str, expected: list[TokenType]):
+    actual_tokens = get_token_types(input_)
+    assert_language_invariants(actual_tokens)
+    assert_token_types_equal(actual_tokens, expected)
+
+
+@pytest.mark.parametrize(
+    "input_, expected",
+    [
+        (
+            """
+struct A:
+    a: int,
+    b: str, d: int
+    c: float
+""",
+            [
+                TokenType.RECORD,
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.NEWLINE,
+                TokenType.INDENT,
+                #
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.IDENTIFIER,
+                TokenType.COMMA,
+                TokenType.NEWLINE,
+                #
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.IDENTIFIER,
+                TokenType.COMMA,
+                #
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.IDENTIFIER,
+                TokenType.NEWLINE,
+                #
+                TokenType.IDENTIFIER,
+                TokenType.COLON,
+                TokenType.IDENTIFIER,
+                TokenType.NEWLINE,
+                TokenType.DEDENT,
+            ],
+        ),
+    ],
+)
+def test_struct_definition(input_: str, expected: list[TokenType]):
+    actual_tokens = get_token_types(input_)
+    assert_language_invariants(actual_tokens)
+    assert_token_types_equal(actual_tokens, expected)
+
+
+@pytest.mark.parametrize(
+    "input_, expected",
+    [
+        (
+            "{1, 2} \\/ {2, 3}",
+            [
+                TokenType.L_BRACE,
+                TokenType.INTEGER,
+                TokenType.COMMA,
+                TokenType.INTEGER,
+                TokenType.R_BRACE,
+                #
+                TokenType.UNION,
+                #
+                TokenType.L_BRACE,
+                TokenType.INTEGER,
+                TokenType.COMMA,
+                TokenType.INTEGER,
+                TokenType.R_BRACE,
+                TokenType.NEWLINE,
+            ],
+        ),
+    ],
+)
+def test_expression(input_: str, expected: list[TokenType]):
+    actual_tokens = get_token_types(input_)
+    assert_language_invariants(actual_tokens)
+    assert_token_types_equal(actual_tokens, expected)
+
+
+@pytest.mark.parametrize(
+    "input_, expected",
+    [
+        (
+            " test",
+            [
+                Token(TokenType.INDENT, "", Location(0, 0), Location(0, 1)),
+                Token(TokenType.IDENTIFIER, "test", Location(0, 1), Location(0, 5)),
+                Token(TokenType.NEWLINE, "", Location(0, 5), Location(1, 0)),
+                Token(TokenType.DEDENT, "", Location(1, 0), Location(1, 0)),
+                Token(TokenType.EOF, "", Location(1, 0), Location(1, 0)),
+            ],
+        ),
+    ],
+)
+def test_tokens_manual(input_: str, expected: list[Token]):
+    res = scan(input_)
+    assert_tokens_equal(res, expected)
+
+
+# MARK: Negative path
